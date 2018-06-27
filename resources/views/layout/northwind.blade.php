bd1f203508d5444eae4ed3c94517800e54a5c607
adrianowarmling <adrianowarmling@unidavi.edu.br>
2018-06-27 18:44:56 -0300

    Reefazendo o laravel

---
+++ resources/views/layout/northwind.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>North Wind</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="css/loja.css" />
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+    
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.php" class="nav-link active">North Wind</a>
+            </div>
+            <div>
+                <ul class="nav nav-pills">
+                    <li class="nav-item"><a href="" class="nav-item nav-link disabled">Cadastro de funcionário</a>
+                </li>
+                    <li class="nav-item"><a href=""class="nav-item nav-link disabled">Cadastro Região</a></li>
+                    <li class="nav-item"><a href=""class="nav-item nav-link disabled">Listar Regiões</a></li>
+                    <li class="nav-item"><a href=""class="nav-item nav-link disabled">Cadastro Território</a></li>
+                    <li class="nav-item"><a href=""class="nav-item nav-link disabled">Listar Territórios</a></li>
+                    <li class="nav-item"><a href=""class="nav-item nav-link disabled">Lista de Funcionários</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="principal"></div>
+        </div>
+        </body>
+        <footer class="page-footer font-small mdb-color lighten-3 pt-4 mt-4">
+        <center>
+            <div class="jumbotron">
+                <div class="container">
+
+                <h5 class="text-uppercase mb-4 font-weight-bold">Address</h5>
+                <!--Info-->
+                <p>
+                    <i class="fa fa-home mr-3"></i> Rio do Sul, SC 89160000, BR</p>
+                <p>
+                    <i class="fa fa-envelope mr-3"></i><a href="mailto:adrianowarmling@unidavi.edu.br">adrianowarmling@unidavi.edu.br</a></p>
+                <p>
+                    <i class="fa fa-phone mr-3"></i> +55 (47)9 8846-4596</p>
+                  
+                </div>
+            </div>
+           
+        </center>
+        </footer>
+   
+</html>
---
+++ rodape.php
@@ -0,0 +1,24 @@
+            </div>
+        </div>
+        </body>
+        <footer class="page-footer font-small mdb-color lighten-3 pt-4 mt-4">
+        <center>
+            <div class="jumbotron">
+                <div class="container">
+
+                <h5 class="text-uppercase mb-4 font-weight-bold">Address</h5>
+                <!--Info-->
+                <p>
+                    <i class="fa fa-home mr-3"></i> Rio do Sul, SC 89160000, BR</p>
+                <p>
+                    <i class="fa fa-envelope mr-3"></i><a href="mailto:adrianowarmling@unidavi.edu.br">adrianowarmling@unidavi.edu.br</a></p>
+                <p>
+                    <i class="fa fa-phone mr-3"></i> +55 (47)9 8846-4596</p>
+                  
+                </div>
+            </div>
+           
+        </center>
+        </footer>
+   
+</html>
